96ee69ac5f5e9c3832ccb7e16cb81c2e2c4fe10f
Stijn de Witt <stijndewitt@foodsy.eu>
2024-02-24 11:31:52 +0100

    1.1.0-beta.5: Support enhanced adapter syntax

--- anylogger-debug.ts
+++ anylogger-debug.ts
@@ -1,27 +1,27 @@
 import anylogger, {
   type LogLevel,
   type Logger,
-  type Extension,
   type Adapter,
 } from 'anylogger'
 import debug from 'debug'
 
-const extension: Extension = function(logger) {
-  var method = debug(logger.name)
-  for (var level in anylogger.levels) {
-    (logger as Logger)[level as LogLevel] = method
-  }
-  (logger as Logger).enabledFor = debug.enabled.bind(logger, logger.name)
-  return logger as Logger
-}
-
-const adapter: Adapter = (anylogger) => {
+const adapter: Adapter = (anylogger, debug) => {
+  // bail early if it was already extended
+  if ((anylogger as any).debug) return
   // override anylogger.ext() to make every log method use debug
-  anylogger.ext = extension
+  anylogger.ext = function(logger) {
+    var method = debug(logger.name)
+    for (var level in anylogger.levels) {
+      (logger as Logger)[level as LogLevel] = method
+    }
+    (logger as Logger).enabledFor = debug.enabled.bind(logger, logger.name)
+    return logger as Logger
+  }
+  // set a flag so we can see it was already extended
+  ;(anylogger as any).debug = debug
 }
 
 export default adapter
 
 // back compat
-adapter(anylogger)
-
+adapter(anylogger, debug)
--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default [
 		input: pkg.main,
 		output: [
       // commonjs build
-			{ file: pkg.cjs,  format: 'cjs', strict: false },
+			{ file: pkg.cjs, format: 'cjs', strict: false },
 		],
 		external: [ 'anylogger', 'debug' ],
 	},
@@ -14,24 +14,8 @@ export default [
 		input: pkg.main,
 		output: [
       // browser-friendly build
-			{ file: pkg.iife,  format: 'iife', strict: false, globals: { anylogger: 'anylogger', debug: 'debug' } },
+			{ file: pkg.iife, name: 'adapter', format: 'iife', strict: false, globals: { anylogger: 'anylogger', debug: 'debug' } },
 		],
 		external: [ 'anylogger', 'debug' ],
-		plugins: [
-			// remove import bloat from iife bundle
-			replace({
-				patterns: [
-					{
-						match: /anylogger-debug/,
-						test: 'import anylogger from \'anylogger\'',
-						replace: '',
-					}, {
-						match: /anylogger-debug/,
-						test: 'import debug from \'debug\'',
-						replace: '',
-					},
-				]
-			})
-		],
 	},
 ];
